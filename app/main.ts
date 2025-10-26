4a13e9e283b5c10dea2716a6105a86d48152462e
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-10-26 23:26:36 +0700

    master: Refactored command handling into functions

--- app/main.ts
+++ app/main.ts
@@ -5,17 +5,34 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+enum Command {
+  EXIT = "exit",
+  ECHO = "echo",
+}
+
+function Exit(answer: string) {
+  if (
+    answer.startsWith(Command.EXIT) ||
+    Number.isInteger(answer[answer.length - 1])
+  ) {
+    const exitCode = +answer[answer.length - 1];
+    rl.close();
+    return exitCode;
+  }
+}
+
+function Echo(answer: string) {
+  if (answer.startsWith(Command.ECHO)) {
+    console.log(answer.replace(`${Command.ECHO} `, ""));
+  } else {
+    console.log(`${answer}: command not found`);
+  }
+}
+
 (function command() {
   rl.question("$ ", (answer) => {
-    if (
-      answer.startsWith("exit") ||
-      Number.isInteger(answer[answer.length - 1])
-    ) {
-      const exitCode = +answer[answer.length - 1];
-      rl.close();
-      return exitCode;
-    }
-    console.log(`${answer}: command not found`);
+    Exit(answer);
+    Echo(answer);
     command();
   });
 })();
