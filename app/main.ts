587aa10a346099b3f6774a28fba36cee270fe632
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-10-26 23:40:45 +0700

    master: Refactored command handling logic

--- app/main.ts
+++ app/main.ts
@@ -5,30 +5,37 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-enum Command {
+enum BuiltInCommand {
   EXIT = "exit",
   ECHO = "echo",
+  TYPE = "type",
 }
 
-function Echo(answer: string) {
-  if (answer.startsWith(Command.ECHO)) {
-    console.log(answer.replace(`${Command.ECHO} `, ""));
-  } else {
-    console.log(`${answer}: command not found`);
-  }
-}
+const builtinCommands = Object.values(BuiltInCommand);
 
 (function command() {
   rl.question("$ ", (answer) => {
+    const answerWords = answer.split(" ");
     if (
-      answer.startsWith(Command.EXIT) ||
+      answer.startsWith(BuiltInCommand.EXIT) ||
       Number.isInteger(answer[answer.length - 1])
     ) {
       const exitCode = +answer[answer.length - 1];
       rl.close();
       return exitCode;
     }
-    Echo(answer);
+
+    if (answerWords.length > 1 && answer.startsWith("type")) {
+      if (builtinCommands.includes(answerWords[1] as BuiltInCommand)) {
+        console.log(`${answerWords[1]} is a shell builtin`);
+      } else {
+        console.log(`${answerWords[1]}: not found`);
+      }
+    } else if (answer.startsWith(BuiltInCommand.ECHO)) {
+      console.log(answer.replace(`${BuiltInCommand.ECHO} `, ""));
+    } else {
+      console.log(`${answer}: command not found`);
+    }
     command();
   });
 })();
